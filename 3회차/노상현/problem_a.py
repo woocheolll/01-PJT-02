16306e598624afaf24db9ec65c936d8753614f3c
hphktutor6 <tutor6@hphk.kr>
2022-07-22 10:00:10 +0900

    init

---
+++ 3회차/노상현/problem_a.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def popular_count():
+    pass 
+    # 여기에 코드를 작성합니다.  
+
+
+# 아래의 코드는 수정하지 않습니다.
+if __name__ == '__main__':
+    """
+    popular 영화목록의 개수 반환
+    """
+    print(popular_count())
+    # 20
